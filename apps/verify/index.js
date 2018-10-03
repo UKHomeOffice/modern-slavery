a64a406b825cce4239d9c0d7c415a51b40809255
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-10-03 23:12:17 +0100

    [MS-232] - Add Gov notify npm notifications-node-client

--- apps/verify/index.js
+++ apps/verify/index.js
@@ -1,12 +1,13 @@
 'use strict';
 const emailLookup = require('./behaviours/email-lookup');
+const emailSender = require('./behaviours/email-sender');
 
 module.exports = {
   name: 'verify',
   steps: {
     '/user-email': {
       fields: ['user-email'],
-      behaviours: [emailLookup],
+      behaviours: [emailLookup, emailSender],
       next: '/check-inbox'
     },
     '/check-inbox': {
