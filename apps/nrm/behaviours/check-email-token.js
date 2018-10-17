f18984eba3699e1251465757eb9e4779e7352c92
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-10-17 11:21:37 +0100

    [MS-232] - Allow users to go back when a token is used
    Previously, if a user clicked on a link and then went back they could not continue with their current session.  This sets a value on the session and then checks that value should a user click back to the first page in the journey

--- apps/nrm/behaviours/check-email-token.js
+++ apps/nrm/behaviours/check-email-token.js
@@ -6,8 +6,11 @@ module.exports = superclass => class extends superclass {
 
   getValues(req, res, callback) {
     const token = req.query.token;
+    const dev = process.env.NODE_ENV === 'development';
 
-    if (token === 'skip' && process.env.NODE_ENV === 'development') {
+    // skips if it goes to /nrm/start?token=skip
+    // skips if a session is already present
+    if (token === 'skip' && dev || (req.sessionModel.get('valid-token')) === true) {
       return super.getValues(req, res, callback);
     }
 
@@ -19,6 +22,8 @@ module.exports = superclass => class extends superclass {
       if (isValidToken) {
         // delete the token once it's been used
         checkToken.delete(token);
+        // this is so a user can go back without requesting a new token
+        req.sessionModel.set('valid-token', true);
         return super.getValues(req, res, callback);
       }
       return res.redirect('/nrm/token-invalid');
