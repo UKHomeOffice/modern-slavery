a767325b3bee662f4be489dfc4c11df4712f5ff7
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-10-23 11:49:56 +0100

    [MS-232] - update govnotify templateId for userAuth
    Name the templateId something more meaningful

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ module.exports = {
   tokenExpiry: 86400,
   govukNotify: {
     notifyApiKey: process.env.NOTIFY_KEY || '',
-    templateId: '56b5a84f-7024-41d3-bd08-26521435be16',
-    appLink: 'https://www.google.co.uk/'
+    templateUserAuthId: '56b5a84f-7024-41d3-bd08-26521435be16',
   }
 };
