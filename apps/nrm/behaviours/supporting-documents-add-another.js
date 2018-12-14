14c0543081ed87fcc52f5739510e2db93fb69ca9
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-12-14 14:52:33 +0000

    MS-667 - Delete - make it a private method

--- apps/nrm/behaviours/supporting-documents-add-another.js
+++ apps/nrm/behaviours/supporting-documents-add-another.js
@@ -1,17 +1,19 @@
 'use strict';
 
+const deleteItem = (req, res) => {
+  const items = req.sessionModel.get('supporting-documents') || [];
+  req.sessionModel.set('supporting-documents',
+    items.filter((file) => file.id !== req.query.delete));
+  // redirect to the original page that called this function
+  res.redirect(req.get('referer'));
+};
+
 module.exports = superclass => class extends superclass {
   get(req, res, callback) {
     if (req.query.delete) {
-      return this.deleteItem(req, res);
+      console.log('in delete');
+      return deleteItem(req, res);
     }
     return super.get(req, res, callback);
-  }
-
-  deleteItem(req, res) {
-    const items = req.sessionModel.get('supporting-documents') || [];
-    req.sessionModel.set('supporting-documents',
-      items.filter((file) => file.id !== req.query.delete));
-    res.redirect(req.get('referer'));
   }
 };
