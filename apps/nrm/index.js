a35fa7dfae8ab4a14227d3758d8e3c1b09db5ecc
julianrobotham <julian.robotham@digital.homeoffice.gov.uk>
2019-02-06 16:23:42 +0000

    MS-714 task: add the skeleton of the app to index.js (NRM)
    Add the remaining pages for the happy path

--- apps/nrm/index.js
+++ apps/nrm/index.js
@@ -13,6 +13,75 @@ module.exports = {
   steps: {
     '/start': {
       behaviours: checkEmailToken,
+      next: '/select-location'
+    },
+    '/select-location': {
+      next: '/pv-under-age'
+    },
+    '/pv-under-age': {
+      next: '/pv-under-age-at-time-of-exploitation'
+    },
+    '/pv-under-age-at-time-of-exploitation': {
+      next: '/what-happened'
+    },
+    '/what-happened': {
+      next: '/where-exploitation-happened'
+    },
+    '/where-exploitation-happened': {
+      next: '/current-pv-location'
+    },
+    '/current-pv-location': {
+      next: '/who-exploited-pv'
+    },
+    '/who-exploited-pv': {
+      next: '/types-of-exploitation'
+    },
+    '/types-of-exploitation': {
+      next: '/any-other-pvs'
+    },
+    '/any-other-pvs': {
+      next: '/reported-to-police'
+    },
+    '/reported-to-police': {
+      next: '/pv-want-to-submit-nrm'
+    },
+    '/pv-want-to-submit-nrm': {
+      next: '/does-pv-need-support'
+    },
+    '/does-pv-need-support': {
+      next: '/pv-name-that-requires-support'
+    },
+    '/pv-name-that-requires-support': {
+      next: '/pv-dob'
+    },
+    '/pv-dob': {
+      next: '/pv-gender'
+    },
+    '/pv-gender': {
+      next: '/does-pv-have-children'
+    },
+    '/does-pv-have-children': {
+      next: '/pv-nationality'
+    },
+    '/pv-nationality': {
+      next: '/pv-interpreter-requirements'
+    },
+    '/pv-interpreter-requirements': {
+      next: '/pv-other-help-with-communication'
+    },
+    '/pv-other-help-with-communication': {
+      next: '/pv-ho-reference'
+    },
+    '/pv-ho-reference': {
+      next: '/pv-contact-details'
+    },
+    '/pv-contact-details': {
+      next: '/pv-phone-number'
+    },
+    '/pv-phone-number': {
+      next: '/co-operate-with-police'
+    },
+    '/co-operate-with-police': {
       next: '/supporting-documents-add'
     },
     '/supporting-documents-add': {
@@ -26,7 +95,7 @@ module.exports = {
           value: 'yes'
         }
       }],
-      next: '/confirm'
+      next: '/fr-details'
     },
     '/supporting-documents': {
       behaviours: supportingDocuments,
@@ -50,6 +119,12 @@ module.exports = {
         }
       }],
       continueOnEdit: true,
+      next: '/fr-details'
+    },
+    '/fr-details': {
+      next: '/fr-alternative-contact'
+    },
+    '/fr-alternative-contact': {
       next: '/confirm'
     },
     '/confirm': {
