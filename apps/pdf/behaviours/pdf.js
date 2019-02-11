73d80e0b10ce5dbbcfbd2f9a0256bea41a4c38cb
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2019-02-11 22:51:23 +0000

    MS-753 PDF Generator - unit tests
    * Move pdf generator function to util folder
    * Create unit tests for pdf generator and pdf behaviour

--- apps/pdf/behaviours/pdf.js
+++ apps/pdf/behaviours/pdf.js
@@ -1,26 +1,18 @@
 'use strict';
-const wkhtmltopdf = require('wkhtmltopdf');
 const config = require('../../../config');
 const htmlTemplate = config.pdf.template;
 const tempLocation = config.pdf.tempLocation;
-const fs = require('fs');
 const uuid = require('uuid');
+const PDFgenerator = require('../util/pdf-generator');
 
 const createTemporaryFileName = () => {
-    return (`${uuid.v1()}.pdf`);
-};
-
-const generatePDF = (html, destination, tempName) => {
-  wkhtmltopdf(fs.createReadStream(html, 'utf8'), {
-      output: `${destination}/${tempName}`
-    });
+  return (`${uuid.v1()}.pdf`);
 };
 
 module.exports = superclass => class extends superclass {
-  // attempt to generate a pdf
   saveValues(req, res, next) {
     const tempName = createTemporaryFileName();
-    generatePDF(htmlTemplate, tempLocation, tempName);
+    PDFgenerator.generate(htmlTemplate, tempLocation, tempName);
 
     super.saveValues(req, res, (err) => {
       next(err);
