79647b527d170166605a05e76656a36ac76f04a7
julianrobotham <julian.robotham@digital.homeoffice.gov.uk>
2019-02-28 03:20:17 +0000

    MS-734 refactor: update acceptance tests + content on form pages
    MS-805 task: update new content changes
    
    Refactor acceptance tests to reduce statements within a function
    
    Add content to Translations directory instead of in the html

--- acceptance-test/user-pathways/happy-path/happy-path.test.js
+++ acceptance-test/user-pathways/happy-path/happy-path.test.js
@@ -27,19 +27,29 @@ let page;
 let url;
 
 /**
- * Select the continue button a number of times specified within the
- * supplied parameters
+ * Click on a page element
  *
- * @param {number} loopCount - the number of time the loop should run
+ * @param {string} selector - selector for element on the page
  *
- * @returns {void}
+ * @returns {Promise}
  */
-const clickContinueButton = async(loopCount) => {
-    for (let i = 0; i < loopCount; i++) {
-            await page.waitForSelector(CONTINUE_BUTTON);
-            await page.click(CONTINUE_BUTTON);
-    }
-};
+async function clickElement(selector) {
+    await page.waitForSelector(selector);
+    await page.click(selector);
+}
+
+/**
+ * Navigate to a page
+ *
+ * @param {string} urlString - the page url
+ *
+ * @returns {Promise}
+ */
+async function navigateTo(urlString) {
+    url = urlString;
+    await page.goto(url);
+    await page.setViewport(VIEWPORT);
+}
 
 describe('Critical user path(s)', () => {
     beforeEach(async() => {
@@ -54,219 +64,106 @@ describe('Critical user path(s)', () => {
     });
 
     /**
-     * Navigate from Who do you work for? Page
+     * Run a sequence of actions to simulate verification of a user
      *
-     * @returns {void}
+     * @returns {Promise}
      */
-    async function whoDoYouWorkForPage() {
-        await page.waitForSelector(ORGANISATION_INPUT);
+    async function verifyUser() {
+        // start
+        await clickElement(CONTINUE_BUTTON);
+        // who-do-you-work-for
         await page.$eval(ORGANISATION_INPUT, (element) => {
             element.value = 'Barnardos';
         });
         await page.$eval(EMAIL_INPUT, (element) => {
             element.value = 'test.user@homeoffice.gov.uk';
         });
-        await clickContinueButton(1);
+        await clickElement(CONTINUE_BUTTON);
+        // Bypass user clicking email link - Notify Key will not be set during test runs
+        await navigateTo(`http://${APP_CONTAINER_HOST}:${APP_CONTAINER_PORT}/nrm/start?token=skip`);
     }
 
     /**
-     * Navigate from Where are you making this report? Page
+     * Run a sequence of actions to simulate the completion of the first half
+     * of the NRM form
      *
-     * @returns {void}
+     * @returns {Promise}
      */
-    async function whereAreYouMakingThisReportPage() {
-        await page.waitForSelector(LOCATION_ENGLAND_OPTION);
-        await page.click(LOCATION_ENGLAND_OPTION);
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Is the potential victim under 18? Page
-     *
-     * @returns {void}
-     */
-    async function isThePotentialVictimUnder18Page() {
-        await page.waitForSelector(PV_UNDER_AGE_NO_OPTION);
-        await page.click(PV_UNDER_AGE_NO_OPTION);
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Were they under 18 at any time during the exploitation?
-     * Page
-     *
-     * @returns {void}
-     */
-    async function wereTheyUnder18AtAnyTimeDuringTheExploitationPage() {
-        await page.waitForSelector(PV_UNDER_AGE_AT_TIME_OF_EXPLOITATION_NO_OPTION);
-        await page.click(PV_UNDER_AGE_AT_TIME_OF_EXPLOITATION_NO_OPTION);
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from What did they say happened? Page
-     *
-     * @returns {void}
-     */
-    async function whatDidTheySayHappenedPage() {
-        await page.waitForSelector(WHAT_HAPPENED_INPUT);
+    async function completeNrmFormPart1() {
+        // nrm start
+        await clickElement(CONTINUE_BUTTON);
+        // fr-location
+        await clickElement(LOCATION_ENGLAND_OPTION);
+        await clickElement(CONTINUE_BUTTON);
+        // pv-under-age
+        await clickElement(PV_UNDER_AGE_NO_OPTION);
+        await clickElement(CONTINUE_BUTTON);
+        // pv-under-age-at-time-of-exploitation
+        await clickElement(PV_UNDER_AGE_AT_TIME_OF_EXPLOITATION_NO_OPTION);
+        await clickElement(CONTINUE_BUTTON);
+        // what-happened
         await page.$eval(WHAT_HAPPENED_INPUT, (element) => {
             element.value = 'Test input regarding details of exploitation';
         });
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Where were they last exploited? Page
-     *
-     * @returns {void}
-     */
-    async function whereWereTheyLastExploitedPage() {
-        await page.waitForSelector(EXPLOITED_IN_UK_OPTION);
-        await page.click(EXPLOITED_IN_UK_OPTION);
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Where are they now? Page
-     *
-     * @returns {void}
-     */
-    async function whereAreTheyNowPage() {
-        await page.waitForSelector(CURRENT_PV_LOCATION_UK_REGION);
+        await clickElement(CONTINUE_BUTTON);
+        // where-exploitation-happened
+        await clickElement(EXPLOITED_IN_UK_OPTION);
+        await clickElement(CONTINUE_BUTTON);
+        // current-pv-location
         await page.$eval(CURRENT_PV_LOCATION_UK_REGION, (element) => {
             element.value = 'Rutland';
         });
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Who exploited them? Page
-     *
-     * @returns {void}
-     */
-    async function whoExploitedThemPage() {
-        await page.waitForSelector(WHO_EXPLOITED_PV);
+        await clickElement(CONTINUE_BUTTON);
+        // who-exploited-pv
         await page.$eval(WHO_EXPLOITED_PV, (element) => {
             element.value = 'Test details about exploiter(s)';
         });
-        await clickContinueButton(1);
+        await clickElement(CONTINUE_BUTTON);
+        // types-of-exploitation
+        await clickElement(CONTINUE_BUTTON);
+        // any-other-pvs
+        await clickElement(ANY_OTHER_PVS_NO_OPTION);
+        await clickElement(CONTINUE_BUTTON);
+        // reported-to-police
+        await clickElement(PV_HAS_CRIME_REFERENCE_NUMBER_YES_OPTION);
+        await clickElement(CONTINUE_BUTTON);
+        // pv-want-to-submit-nrm
+        await clickElement(REFER_CASE_TO_NRM_YES_OPTION);
+        await clickElement(CONTINUE_BUTTON);
     }
 
     /**
-     * Navigate from How were they exploited? Page
-     *
-     * @returns {void}
+     * Run a sequence of actions to simulate the completion of the second half
+     * of the NRM form
      */
-    async function howWereTheyExploitedPage() {
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Were there any other potential victims? Page
-     *
-     * @returns {void}
-     */
-    async function wereThereAnyOtherPotentialVictimsPage() {
-        await page.waitForSelector(ANY_OTHER_PVS_NO_OPTION);
-        await page.click(ANY_OTHER_PVS_NO_OPTION);
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Do they have a crime reference number? Page
-     *
-     * @returns {void}
-     */
-    async function doTheyHaveACrimeReferenceNumberPage() {
-        await page.waitForSelector(PV_HAS_CRIME_REFERENCE_NUMBER_YES_OPTION);
-        await page.click(PV_HAS_CRIME_REFERENCE_NUMBER_YES_OPTION);
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Do they want their case referred to the NRM? Page
-     *
-     * @returns {void}
-     */
-    async function doTheyWantTheirCaseReferredToTheNrmPage() {
-        await page.waitForSelector(REFER_CASE_TO_NRM_YES_OPTION);
-        await page.click(REFER_CASE_TO_NRM_YES_OPTION);
-        await clickContinueButton(1);
-    }
-
-    /**
-     * Navigate from Do you want to upload supporting documents? Page
-     *
-     * @returns {void}
-     */
-    async function doYouWantToUploadSupportingDocumentsPage() {
-        await page.waitForSelector(UPLOAD_DOCUMENT_PAGE_2_NO_OPTION);
-        await page.click(UPLOAD_DOCUMENT_PAGE_2_NO_OPTION);
-        await clickContinueButton(1);
+    async function completeNrmFormPart2() {
+        // Run through the skeleton until we reach the upload page
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        // supporting-documents-add
+        await clickElement(UPLOAD_DOCUMENT_PAGE_2_NO_OPTION);
+        await clickElement(CONTINUE_BUTTON);
+        // Run through the skeleton until we reach the end
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
+        await clickElement(CONTINUE_BUTTON);
     }
 
     it('Happy path - Adult', async() => {
         try {
-            // start
-            await clickContinueButton(1);
-
-            // who-do-you-work-for
-            await whoDoYouWorkForPage();
-
-            // Bypass user clicking email link - Notify Key will not be set during test runs
-            url = `http://${APP_CONTAINER_HOST}:${APP_CONTAINER_PORT}/nrm/start?token=skip`;
-            await page.goto(url);
-
-            // Hit the url a second time since the first page resolves to an error
-            await page.goto(url);
-            await page.setViewport(VIEWPORT);
-
-            // nrm start
-            await clickContinueButton(1);
-
-            // fr-location
-            await whereAreYouMakingThisReportPage();
-
-            // pv-under-age
-            await isThePotentialVictimUnder18Page();
-
-            // pv-under-age-at-time-of-exploitation
-            await wereTheyUnder18AtAnyTimeDuringTheExploitationPage();
-
-            // what-happened
-            await whatDidTheySayHappenedPage();
-
-            // where-exploitation-happened
-            await whereWereTheyLastExploitedPage();
-
-            // current-pv-location
-            await whereAreTheyNowPage();
-
-            // who-exploited-pv
-            await whoExploitedThemPage();
-
-            // types-of-exploitation
-            await howWereTheyExploitedPage();
-
-            // any-other-pvs
-            await wereThereAnyOtherPotentialVictimsPage();
-
-            // reported-to-police
-            await doTheyHaveACrimeReferenceNumberPage();
-
-            // pv-want-to-submit-nrm
-            await doTheyWantTheirCaseReferredToTheNrmPage();
-
-            // Run through the skeleton until we reach the upload page
-            await clickContinueButton(12);
-
-            // supporting-documents-add
-            await doYouWantToUploadSupportingDocumentsPage();
-
-            // Run through the skeleton until we reach the end
-            await clickContinueButton(3);
-
+            await verifyUser();
+            await completeNrmFormPart1();
+            await completeNrmFormPart2();
         } catch (err) {
             throw new Error(err);
         }
