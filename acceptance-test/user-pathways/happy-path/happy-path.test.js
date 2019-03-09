573a59aac9b7f0d383d4fa01d7bffe6972a4a312
julianrobotham <julian.robotham@digital.homeoffice.gov.uk>
2019-03-09 14:47:11 +0000

    MS-happy-path task: remove comments from tests

--- acceptance-test/user-pathways/happy-path/happy-path.test.js
+++ acceptance-test/user-pathways/happy-path/happy-path.test.js
@@ -90,9 +90,7 @@ describe.only('User path(s)', () => {
      * @returns {Promise}
      */
     async function verifyUser() {
-        // start
         await clickSelector(page, CONTINUE_BUTTON);
-        // who-do-you-work-for
         await page.$eval(ORGANISATION_INPUT, (element) => {
             element.value = 'Barnardos';
         });
@@ -120,24 +118,18 @@ describe.only('User path(s)', () => {
      * @returns {Promise}
      */
     async function completeNrmFormPart1(typeOfPV, caseReferred) {
-        // nrm start
         await clickSelector(page, CONTINUE_BUTTON);
-        // fr-location
         await clickSelector(page, LOCATION_ENGLAND_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
 
         if (typeOfPV === 'adult') {
-            // pv-under-age
             await clickSelector(page, PV_UNDER_AGE_NO_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // pv-under-age-at-time-of-exploitation
             await clickSelector(page, PV_UNDER_AGE_AT_TIME_OF_EXPLOITATION_NO_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
         } else {
-            // pv-under-age
             await clickSelector(page, PV_UNDER_AGE_YES_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // local-authority-contacted-about-child
             await page.waitForSelector(LOCAL_AUTHORITY_NAME);
             await page.$eval(LOCAL_AUTHORITY_NAME, (element) => {
                 element.value = 'Local Authority A';
@@ -151,51 +143,38 @@ describe.only('User path(s)', () => {
             await clickSelector(page, CONTINUE_BUTTON);
         }
 
-        // what-happened
         await page.waitForSelector(WHAT_HAPPENED_INPUT);
         await page.$eval(WHAT_HAPPENED_INPUT, (element) => {
             element.value = 'Test input regarding details of exploitation';
         });
         await clickSelector(page, CONTINUE_BUTTON);
-        // where-exploitation-happened
         await clickSelector(page, EXPLOITED_IN_UK_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
-        // current-pv-location
         await page.$eval(CURRENT_PV_LOCATION_UK_REGION, (element) => {
             element.value = 'Rutland';
         });
         await clickSelector(page, CONTINUE_BUTTON);
-        // who-exploited-pv
         await page.$eval(WHO_EXPLOITED_PV, (element) => {
             element.value = 'Test details about exploiter(s)';
         });
         await clickSelector(page, CONTINUE_BUTTON);
-        // types-of-exploitation
         await clickSelector(page, CONTINUE_BUTTON);
-        // any-other-pvs
         await clickSelector(page, ANY_OTHER_PVS_NO_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
-        // reported-to-police
         await clickSelector(page, PV_HAS_CRIME_REFERENCE_NUMBER_YES_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
 
         if (caseReferred && typeOfPV === 'adult') {
-            // pv-want-to-submit-nrm
             await clickSelector(page, REFER_CASE_TO_NRM_YES_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // does-pv-need-support
             await clickSelector(page, DOES_PV_NEED_SUPPORT_NO_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
         } else if (!caseReferred && typeOfPV === 'adult') {
-            // pv-want-to-submit-nrm
             await clickSelector(page, REFER_CASE_TO_NRM_NO_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // refuse-nrm
             await clickSelector(page, CONTINUE_BUTTON);
-            // refuse-nrm-co-operate-with-police
             await clickSelector(page, REFUSE_NRM_POLICE_CONTACT_YES_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // refuse-nrm-pv-name
             await page.waitForSelector(REFUSE_NRM_PV_NAME_FIRST_NAME);
             await page.$eval(REFUSE_NRM_PV_NAME_FIRST_NAME, (element) => {
                 element.value = 'Robert';
@@ -204,7 +183,6 @@ describe.only('User path(s)', () => {
                 element.value = 'Maxwell';
             });
             await clickSelector(page, CONTINUE_BUTTON);
-            // refuse-nrm-pv-contact-details
             await clickSelector(page, REFUSE_NRM_PV_CONTACT_DETAILS_EMAIL_OPTION);
             await page.waitForSelector(REFUSE_NRM_PV_CONTACT_DETAILS_EMAIL_INPUT);
             await page.$eval(REFUSE_NRM_PV_CONTACT_DETAILS_EMAIL_INPUT, (element) => {
@@ -212,7 +190,6 @@ describe.only('User path(s)', () => {
             });
             await clickSelector(page, REFUSE_NRM_PV_CONTACT_DETAILS_EMAIL_SAFE_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // summary page
             await clickSelector(page, CONTINUE_BUTTON);
         }
     }
@@ -231,7 +208,6 @@ describe.only('User path(s)', () => {
      * @returns {Promise}
      */
     async function completeNrmFormPart2(typeOfPV) {
-        // pv-name-that-requires-support
         await page.waitForSelector(PV_NAME_REQUIRING_SUPPORT_FIRST_NAME);
         await page.$eval(PV_NAME_REQUIRING_SUPPORT_FIRST_NAME, (element) => {
             element.value = 'Paul';
@@ -240,32 +216,24 @@ describe.only('User path(s)', () => {
             element.value = 'Shortlands';
         });
         await clickSelector(page, CONTINUE_BUTTON);
-        // pv-dob
         await clickSelector(page, CONTINUE_BUTTON);
-        // pv-gender
         await clickSelector(page, PV_GENDER_MALE_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
-        // does-pv-have-children
         await clickSelector(page, DOES_PV_HAVE_CHILDREN_NO_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
-        // pv-nationality
         await page.waitForSelector(PV_NATIONALITY);
         await page.$eval(PV_NATIONALITY, (element) => {
             element.value = 'United Kingdom';
         });
         await clickSelector(page, CONTINUE_BUTTON);
-        // pv-interpreter-requirements
         await clickSelector(page, INTERPRETER_NO_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
-        // pv-other-help-with-communication
         await clickSelector(page, COMMUNICATION_AID_NO_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
-        // pv-ho-reference
         await clickSelector(page, HO_REFERENCE_NO_OPTION);
         await clickSelector(page, CONTINUE_BUTTON);
 
         if (typeOfPV === 'adult') {
-            // pv-contact-details
             await clickSelector(page, PV_CONTACT_DETAILS_EMAIL_OPTION);
             await page.waitForSelector(PV_CONTACT_DETAILS_EMAIL_INPUT);
             await page.$eval(PV_CONTACT_DETAILS_EMAIL_INPUT, (element) => {
@@ -273,15 +241,12 @@ describe.only('User path(s)', () => {
             });
             await clickSelector(page, PV_CONTACT_DETAILS_EMAIL_SAFE_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // pv-phone-number
             await clickSelector(page, PV_PHONE_NUMBER_NO_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
-            // co-operate-with-police
             await clickSelector(page, POLICE_CONTACT_YES_OPTION);
             await clickSelector(page, CONTINUE_BUTTON);
         }
 
-        // fr-details
         await page.waitForSelector(FR_DETAILS_NAME_INPUT);
         await page.$eval(FR_DETAILS_NAME_INPUT, (element) => {
             element.value = 'Jack Smith';
@@ -293,13 +258,11 @@ describe.only('User path(s)', () => {
             element.value = '02086757436';
         });
         await clickSelector(page, CONTINUE_BUTTON);
-        // fr-alternative-contact
         await page.waitForSelector(FR_ALTERNATE_CONTACT_EMAIL_INPUT);
         await page.$eval(FR_ALTERNATE_CONTACT_EMAIL_INPUT, (element) => {
             element.value = 'jack.smith@police.com';
         });
         await clickSelector(page, CONTINUE_BUTTON);
-        // summary page
         await clickSelector(page, CONTINUE_BUTTON);
     }
 
