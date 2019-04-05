2f2dd104aaaa4adbc01ae0e800c5d6d89db3d25a
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2019-04-05 12:39:18 +0100

    MS-971 PDF: update format and pass in missing data

--- apps/nrm/util/transform-data.js
+++ apps/nrm/util/transform-data.js
@@ -24,18 +24,42 @@ module.exports = {
     data.hostUrl = hostUrl;
     data.date = moment().format('DD/MM/YYYY h:mm a');
     data.reportId = uuidv4();
+    data.reportLocation = upperCase(values['fr-location']);
+    if (data.reportLocation === 'Northern-ireland') {
+      data.reportLocation = 'Northern Ireland';
+    }
     data.exploitationUk = equal(values['where-exploitation-happened'], 'uk');
     data.isChild = equal(values['pv-under-age'], 'yes');
     data.isAdult = equal(values['pv-under-age'], 'no');
+    data.isUnderAge = values['pv-under-age'];
+    if (data.isUnderAge === 'not-sure') {
+      data.isUnderAge = 'not sure';
+      data.isChild = true;
+    }
+    data.underAgeWhenExploited = values['pv-under-age-at-time-of-exploitation'];
+    if (data.underAgeWhenExploited === 'not-sure') {
+      data.underAgeWhenExploited = 'not sure';
+    }
     data.labourExploit = util.compare(['types-of-exploitation-forced-to-work',
       'types-of-exploitation-wages-taken', 'types-of-exploitation-forced-to-commit-fraud'], values);
     data.sexualExploit = util.compare(['types-of-exploitation-prostitution',
       'types-of-exploitation-child-exploitation', 'types-of-exploitation-taken-somewhere'], values);
     data.criminalExploit = util.compare(['types-of-exploitation-forced-to-commit-crime',
       'types-of-exploitation-organs-removed'], values);
+    data.otherVictims = values['any-other-pvs'];
+    if (data.otherVictims === 'not-sure') {
+      data.otherVictims = 'not sure';
+    }
     data.pvWantToSubmit = equal(values['pv-want-to-submit-nrm'], 'yes');
     data.pvNotWantToSubmit = equal(values['pv-want-to-submit-nrm'], 'no');
     data.contactSomeoneElse = equal(values['who-contact'], 'someone-else');
+    data.whoContact = values['who-contact'];
+    if (data.contactSomeoneElse) {
+      data.whoContact = 'Someone else';
+    }
+    if (data.whoContact === 'potential-victim') {
+      data.whoContact = 'Potenital victim';
+    }
     data.pvHasChildren = upperCase(values['does-pv-have-children']);
     data.values = values;
     return data;
