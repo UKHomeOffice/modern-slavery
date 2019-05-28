0e21ae09dd916ca8ba26193bddaa5632e1aba62c
julianrobotham <julian.robotham@digital.homeoffice.gov.uk>
2019-05-28 10:03:09 +0100

    MS-655 task: add connection name to redis Object
    Improves traceability within logs

--- redis.js
+++ redis.js
@@ -5,7 +5,8 @@ const config = require('./config');
 
 const redis = new Redis({
   port: config.redis.port,
-  host: config.redis.host
+  host: config.redis.host,
+  connectionName: 'modern-slavery-app',
 });
 
 module.exports = redis;
