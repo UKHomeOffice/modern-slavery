11911405c5f5ea8623fd998b74dc29ebe4990d6a
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2019-12-18 11:46:44 +0000

    MS-1507 Liberalise email validation
    The email validation bundled with hof is overly restrictive.
    This commit liberalises the validation to essentially accept anything
    that has one @ sign and at least one dot in the domain portion of the
    address.
    
    A particular problem with the old code is that it wouldn't accept
    apostrophes in the username portion; this commit fixes that.

--- apps/verify/fields/index.js
+++ apps/verify/fields/index.js
@@ -2,6 +2,8 @@
 
 const organisations = require('ms-organisations');
 
+const { isValidEmail } = require('../../common/validators.js');
+
 module.exports = {
   'user-organisation': {
     mixin: 'select',
@@ -14,10 +16,10 @@ module.exports = {
   },
   'user-email': {
     mixin: 'input-text',
-    validate: ['required', 'email', {'type': 'maxlength', 'arguments': [15000]}],
+    validate: ['required', isValidEmail, {'type': 'maxlength', 'arguments': [15000]}],
   },
   'confirm-email': {
     mixin: 'input-text',
-    validate: ['required', 'email', {'type': 'maxlength', 'arguments': [15000]}]
+    validate: ['required', isValidEmail, {'type': 'maxlength', 'arguments': [15000]}]
   },
 };
---
+++ test/unit/common/validators.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const validators = require('../../../apps/common/validators');
+
+describe('apps/common/validators', () => {
+
+  it('should reject emails with spaces', () => {
+   const result = [
+    ' test@example.com',
+    'test@example.com ',
+    'test @ example.com',
+    ' test@example.com ',
+    't est @ example.com',
+    'test\t@example.com'
+   ].map(validators.isValidEmail);
+    result.should.not.contain(true);
+  });
+
+  it('should accept emails with strange domain portions', () => {
+    const result = [
+      'test@example.com',
+      'test@example.gov.uk',
+      'test@subdomain.example.com',
+      'test@example.international',
+      'test@example.xn--kgbechtv',
+      'test@example.测试',
+    ].map(validators.isValidEmail);
+    result.should.not.contain(false);
+  });
+
+  it('should accept emails with strange username portions', () => {
+    const result = [
+      'firstname.lastname@example.com',
+      'FirstName.LastName@example.com',
+      'firstname.o\'lastname@example.com',
+      '"name"@example.com'
+    ].map(validators.isValidEmail);
+    result.should.not.contain(false);
+  });
+
+});
