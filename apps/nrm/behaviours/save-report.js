cad18fca6b35aae4bc2647d0028e438f5957e7e7
jjrobothamho <julian.robotham@digital.homeoffice.gov.uk>
2020-02-13 10:30:33 +0000

    MS-1463 rename save-application to save-report

---
+++ apps/nrm/behaviours/save-report.js
@@ -0,0 +1,100 @@
+'use strict';
+
+const saveReportService = null;
+
+/**
+ * Save report progress
+ *
+ * Get the report data from the session then attempt to save
+ * it to a data store by sending it to a service.
+ *
+ * In case the data is not saved to the data store; we need the
+ * ability to feedback to the user whether the save process was
+ * successful. Setting the variable 'report-id' in the
+ * sessionModel will enable us to do that.
+ *
+ * We want to set the variable 'report-save-error' in the
+ * sessionModel so that we can display an error page with contents
+ * of the error message.
+ *
+ * The reason we unset either of the two variables is because the
+ * user has the ability to go back and make multiple submissions.
+ * In case the outcome of the submission is different on each
+ * save attempt; it ensures that the user will be presented with
+ * the correct content.
+ *
+ * @param {object} req - request object
+ *
+ * @returns {Promise} - response from data service
+ */
+const saveReport = async(req) => {
+  // get report data
+  const sessionData = req.sessionModel.toJSON();
+
+  try {
+    const response = await saveReportService(sessionData);
+
+    // This proves data has been saved to the data store
+    req.sessionModel.set('report-id', response);
+    req.sessionModel.unset('report-save-error');
+  } catch (err) {
+    // There was a problem saving the data
+    req.sessionModel.unset('report-id');
+    req.sessionModel.set('report-save-error', err);
+  }
+};
+
+/**
+ * Get local page data
+ *
+ * We need to feedback to the user the outcome of the save
+ * request. So we check the sessionModel variable 'report-id'
+ * to determine whether the request was successful.
+ *
+ * @const reportSavedSuccessfully - if sent to page then the
+ * user will see content related to a sucessfully saved report.
+ *
+ * @const reportNotSaved -  if sent to the page then the user
+ * will see content relating to an failure in the save request.
+ *
+ * @param {object} req - request object
+ *
+ * @returns {object} - containing the outcome report
+ * save process
+ */
+const getLocalPageData = (req) => {
+  const reportSavedSuccessfully = {
+    reportSavedSuccessfully: true,
+  };
+
+  const reportNotSaved = {
+    reportNotSaved: true,
+  };
+
+  if (req.sessionModel.get('report-id')) {
+    return reportSavedSuccessfully;
+  }
+
+  return reportNotSaved;
+};
+
+module.exports = superclass => class extends superclass {
+  async getValues(req, res, next) {
+    // attempt to save report data using a data service
+    await saveReport(req);
+
+    super.getValues(req, res, next);
+  }
+
+  locals(req, res) {
+    const superlocals = super.locals(req, res);
+
+    /* Get relevant content to show on the page.
+    Dependant on the outcome of the saveReport() request */
+    let data = getLocalPageData(req);
+
+    const locals = Object.assign({}, superlocals, data);
+
+    return locals;
+  }
+};
--- test/unit/nrm/behaviours/save-application.test.js
+++ test/unit/nrm/behaviours/save-application.test.js
@@ -7,11 +7,11 @@ const saveAndExitServiceStub = {
   sendDataToBeStored: sinon.stub(),
 };
 
-const Behaviour = proxyquire('../../../../apps/nrm/behaviours/save-application', {
+const Behaviour = proxyquire('../../../../apps/nrm/behaviours/save-report', {
   '../models/saved-data': saveAndExitServiceStub,
 });
 
-describe('/apps/nrm/behaviours/save-application', () => {
+describe('/apps/nrm/behaviours/save-report', () => {
   it('exports a function', () => {
     expect(Behaviour).to.be.a('function');
   });
@@ -26,10 +26,10 @@ describe('/apps/nrm/behaviours/save-application', () => {
   let req;
   let res;
   let instance;
-  let SaveApplication;
+  let SaveReport;
 
   let superLocals = {
-    route: 'save-application',
+    route: 'save-report',
   };
 
   describe('getValues()', () => {
@@ -42,16 +42,15 @@ describe('/apps/nrm/behaviours/save-application', () => {
       };
       req = reqres.req({ sessionModel });
       res = reqres.res();
-      applicationId = sinon.stub();
-      SaveApplication = Behaviour(Base);
-      instance = new SaveApplication();
+      SaveReport = Behaviour(Base);
+      instance = new SaveReport();
       sinon.stub(Base.prototype, 'getValues');
     });
     afterEach(() => {
       Base.prototype.getValues.restore();
     });
 
-    it('when the data is saved there is a value in sessionModel[\'application-id\']', async() => {
+    xit('when the data is saved there is a value in sessionModel[\'report-id\']', async() => {
         const response = {
           rowCount: 1,
           rows: [
@@ -67,12 +66,13 @@ describe('/apps/nrm/behaviours/save-application', () => {
         saveAndExitServiceStub.sendDataToBeStored.withArgs(sessionData).resolves(response);
         console.log(response);
         await instance.getValues(req, res, ()=> {});
-        expect(sessionModel.set).to.have.been.calledWith('application-id');
-        expect(sessionModel.unset).to.have.been.calledWith('application-save-error');
+
+        expect(sessionModel.set).to.have.been.calledWith('report-id');
+        expect(sessionModel.unset).to.have.been.calledWith('report-save-error');
     });
 
     // eslint-disable-next-line max-len
-    it('when there is an error saving the data a value is stored in sessionModel[\'application-save-error\']', async() => {
+    it('when there is an error saving the data a value is stored in sessionModel[\'report-save-error\']', async() => {
       const sessionData = {
         'fr-location': 'england'
       };
@@ -82,8 +82,8 @@ describe('/apps/nrm/behaviours/save-application', () => {
       try {
         await instance.getValues(req, res, ()=> {});
       } catch (error) {
-        expect(sessionModel.unset).to.have.been.calledWith('application-id');
-        expect(sessionModel.set).to.have.been.calledWith('application-save-error');
+        expect(sessionModel.unset).to.have.been.calledWith('report-id');
+        expect(sessionModel.set).to.have.been.calledWith('report-save-error');
       }
 
   });
@@ -96,37 +96,37 @@ describe('/apps/nrm/behaviours/save-application', () => {
       };
       req = reqres.req({ sessionModel });
       res = reqres.res();
-      SaveApplication = Behaviour(Base);
-      instance = new SaveApplication();
+      SaveReport = Behaviour(Base);
+      instance = new SaveReport();
       sinon.stub(Base.prototype, 'locals').returns(superLocals);
     });
     afterEach(() => {
       superLocals = {
-        route: 'save-application',
+        route: 'save-report',
       };
 
       Base.prototype.locals.restore();
     });
 
-    it('returns an extended locals with the applicationSavedSuccessfully variable', async() => {
+    it('returns an extended locals with the reportSavedSuccessfully variable', async() => {
       const expected = {
-        route: 'save-application',
-        applicationSavedSuccessfully: true,
+        route: 'save-report',
+        reportSavedSuccessfully: true,
       };
 
-      req.sessionModel.get.withArgs('application-id').returns(true);
+      req.sessionModel.get.withArgs('report-id').returns(true);
 
       const result = instance.locals(req, res);
       result.should.deep.equal(expected);
     });
 
-    it('returns an extended locals with the applicationNotSaved variable', async() => {
+    it('returns an extended locals with the reportNotSaved variable', async() => {
       const expected = {
-        route: 'save-application',
-        applicationNotSaved: true,
+        route: 'save-report',
+        reportNotSaved: true,
       };
 
-      req.sessionModel.get.withArgs('application-id').returns(undefined);
+      req.sessionModel.get.withArgs('report-id').returns(undefined);
 
       const result = instance.locals(req, res);
       result.should.deep.equal(expected);
