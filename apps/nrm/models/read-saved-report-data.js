fe55bac381376aae5a2addf0f67a492c0d2835b6
Julian Robotham <33282783+jjrobothamho@users.noreply.github.com>
2020-02-14 09:35:37 +0000

    MS-1463 add read functionality with data-service api (#376)
    * MS-1463 add read functionality with data-service api
    
    * MS-1463 update comments in getReports function
    
    * MS-1463 fix typo in getReports()

---
+++ apps/nrm/models/read-saved-report-data.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const dataService = require('./data-service');
+const {
+makeRequest,
+requestHeaders,
+} = dataService;
+
+/**
+ * Read data from data service
+ *
+ * @param {number} userEmail - the user email
+ *
+ * @returns {Promise} - response from data service (the row data)
+ */
+const readDataFromStore = async(userEmail) => {
+  const body = {
+    userEmail,
+  };
+
+  const options = {
+    method: 'get',
+    body: JSON.stringify(body),
+    headers: requestHeaders,
+  };
+
+  const response = await makeRequest('/reports/findByEmail', options);
+
+  if (response) {
+    // get the latest report saved by user for now
+    const latestReport = response[response.length - 1];
+    const savedData = latestReport ? JSON.parse(latestReport) : null;
+
+    return savedData;
+  }
+
+  return response;
+};
+
+module.exports = readDataFromStore;
