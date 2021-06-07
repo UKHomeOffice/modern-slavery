2382c954f8ac826e2839c60d18b8ac8fb3748479
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-06-07 09:44:02 +0100

    SD-118: Saving a report shows wrong due date of completion
    	* Changed save-and-exit.js to include calculation for end date instead of start date on the report.
    	* Corrected features to feature typo error in drone.yml
    	* Refactored save-and-exit.js to split out expiry day calculation from Object.
    	* Added unit test for the save-and-exit behaviour, testing date calculation logic.
    	* Added additional unit tests for the 29th of Feb, and for when the created_at date is empty
    	* Removed line space in Readme
    	* Changed UserEmail in save-and-exit.test.js to a proper use case

--- apps/nrm/behaviours/save-and-exit.js
+++ apps/nrm/behaviours/save-and-exit.js
@@ -1,12 +1,18 @@
 'use strict';
 const moment = require('moment');
+const config = require('../../../config');
+
+const calculateExpiryDate = createdAtDate => {
+  return moment(createdAtDate).add(config.reports.deletionTimeout, 'days')
+      .format('DD MMMM YYYY');
+};
 
 module.exports = superclass => class extends superclass {
 
   locals(req, res) {
     const superlocals = super.locals(req, res);
     const data = Object.assign({}, {
-      reportExpiration: moment(req.sessionModel.get('created_at')).format('DD MMMM YYYY'),
+      reportExpiration: calculateExpiryDate(req.sessionModel.get('created_at')),
       userEmail: req.sessionModel.get('user-email')
     });
     const locals = Object.assign({}, superlocals, data);
