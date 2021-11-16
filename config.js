1a5203fd07323fd87e10623c50ab55cefdb08d69
sulthan.ahmed <sulthan.ahmed@digital.homeoffice.gov.uk>
2021-11-16 16:20:50 +0000

    WIP

--- config.js
+++ config.js
@@ -22,7 +22,8 @@ module.exports = {
   env,
   allowSkip: String(process.env.ALLOW_SKIP) === 'true',
   skipEmail: process.env.SKIP_EMAIL,
-  writeToCasework: env !== 'local',
+  writeToCasework: process.env.WRITE_TO_CASEWORK || env !== 'local',
+  // writeToCasework: true,
   hostUrl: process.env.HOST_URL || 'http://localhost:8081',
   saveService: {
     port: process.env.DATASERVICE_SERVICE_PORT_HTTPS || '3000',
