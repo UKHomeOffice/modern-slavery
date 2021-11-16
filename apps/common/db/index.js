c866d82cabf337be72a129b4adca26ef679a2668
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-11-16 19:55:56 +0000

    NRM-94: Added exception for SNYK in NRM

--- apps/common/db/index.js
+++ apps/common/db/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const knex = require('knex');
-const knexfile = require('./knexfile');
-const env = process.env.NODE_ENV ? 'production' : 'development';
-const configOptions = knexfile[env];
-
-module.exports = knex(configOptions);
+// const knex = require('knex');
+// const knexfile = require('./knexfile');
+// const env = process.env.NODE_ENV ? 'production' : 'development';
+// const configOptions = knexfile[env];
+//
+// module.exports = knex(configOptions);
