81f75597af235b84ed2183d1f3e1e2314ac46352
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2021-12-10 20:02:44 +0000

    Fix summary page links depending on if referral or dtn

--- apps/nrm/behaviours/automatic-referral.js
+++ apps/nrm/behaviours/automatic-referral.js
@@ -12,6 +12,10 @@ module.exports = superclass => class extends superclass {
     if (req.sessionModel.get('automatic-referral')) {
       req.form.values['pv-want-to-submit-nrm'] = 'yes';
     }
+    const isReferral = req.form.values['pv-want-to-submit-nrm'] === 'yes';
+
+    req.sessionModel.set('is-referral', isReferral);
+
     return super.saveValues(req, res, next);
   };
 };
--- apps/nrm/behaviours/set-referral-state.js
+++ apps/nrm/behaviours/set-referral-state.js
@@ -3,12 +3,17 @@
 module.exports = superclass => class extends superclass {
   saveValues(req, res, next) {
     const pvUnderAge = req.form.values['pv-under-age'] !== 'no';
+    const previouslyAnsweredSubmitToNRM = req.sessionModel.get('steps').includes('/pv-want-to-submit-nrm');
 
     req.sessionModel.set('automatic-referral', pvUnderAge);
 
     if (pvUnderAge) {
       req.sessionModel.set('pv-want-to-submit-nrm', 'yes');
+      req.sessionModel.set('is-referral', true);
       req.sessionModel.unset('does-pv-need-support');
+    } else if (!previouslyAnsweredSubmitToNRM) {
+      req.sessionModel.unset('pv-want-to-submit-nrm');
+      req.sessionModel.unset('is-referral');
     }
 
     return super.saveValues(req, res, next);
