4cf7224b3220bde7c69e1ed488aec137c3eef572
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2023-02-27 13:26:36 +0000

    change pg version in knexfile.js

--- apps/common/db/knexfile.js
+++ apps/common/db/knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    version: '8.7.1',
+    version: '8.8.0',
     connection: {
       host: config.audit.host,
       user: config.audit.user,
