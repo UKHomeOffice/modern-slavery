3dfdd8b14b84a0d221f3a50e6e52b839762458ab
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2024-04-12 18:37:05 +0100

    [NRM-255] changed value to StartHomeButton varable

--- acceptance-test/test-config.js
+++ acceptance-test/test-config.js
@@ -2,8 +2,9 @@
 
 module.exports = {
   selectors: {
-    START_HOME_BUTTON: '#content > div > form > div > div.govuk-grid-column-two-thirds > input',
-    START_REPORT: '#gds-button',
+    //START_HOME_BUTTON: '#content > div > form > div > div.govuk-grid-column-two-thirds > input',
+    START_HOME_BUTTON: '#gds-button',
+    START_REPORT: '#content > div > form > div > div > input',
     CONTINUE_BUTTON: '#content > div > form > input.button',
     DOWNLOAD_REPORT: '#paper-version-download',
     REFERENCE_INPUT: '#reference',
