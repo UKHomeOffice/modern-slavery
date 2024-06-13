f372471eac876722a1b144e574a03cedff612eeb
Adesh-Ramchurn <163299017+Adesh-Ramchurn@users.noreply.github.com>
2024-06-13 13:01:42 +0100

    reverted manually

--- acceptance-test/user-pathways/happy-path/happy-path.test.js
+++ acceptance-test/user-pathways/happy-path/happy-path.test.js
@@ -81,7 +81,6 @@ let APP_CONTAINER_HOST;
 
 let browser;
 let page;
-let client;
 
 /**
  * .only method used to run only tests within this describe function
@@ -101,8 +100,7 @@ describe.only('User path(s)', () => {
     /* Clear browser cookies before start of each test.
         This so we do not hit the invalid token page when running
         subsequent tests */
-    client = await page.target().createCDPSession();
-    await client.send('Network.clearBrowserCookies');
+    await page._client.send('Network.clearBrowserCookies');
     await page.goto(initialUrl);
   });
 
@@ -347,8 +345,7 @@ describe.only('User path(s)', () => {
 
   it('downloads the prompt sheet', async () => {
     try {
-      client = await page.target().createCDPSession();
-      await client.send('Page.setDownloadBehavior', {
+      await page._client.send('Page.setDownloadBehavior', {
         behavior: 'allow',
         downloadPath: downloadPath
       });
