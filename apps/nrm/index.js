f55ed3095be90ef8d49ae9433fc2aaeaddf1a14c
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-09-12 09:01:17 +0100

    move modify in continue-report

--- apps/nrm/index.js
+++ apps/nrm/index.js
@@ -749,8 +749,8 @@ module.exports = {
       sections: require('./sections/summary-data-sections'),
       backLink: false,
       behaviours: [
-        require('hof').components.summary,
         modifySummaryPage,
+        require('hof').components.summary,
         fullWidth,
         continueReport
       ],
