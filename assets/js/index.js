58b343f29ecd3dc763792a85db06f8c1e96ee380
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-09-12 14:04:21 +0100

    change accessible-autocomplete to typeahead aria

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,74 @@
-/* eslint-disable no-var, vars-on-top */
+/* eslint-disable no-var, vars-on-top consistent-return */
 'use strict';
 
 require('hof/frontend/themes/gov-uk/client-js');
 const $ = require('jquery');
 const govuk = require('govuk-frontend');
-const accessibleAutocomplete = require('accessible-autocomplete');
+const typeahead = require('typeahead-aria');
+const Bloodhound = require('typeahead-aria').Bloodhound;
+
+typeahead.loadjQueryPlugin();
 
 $('.typeahead').each(function applyTypeahead() {
-  accessibleAutocomplete.enhanceSelectElement({
-    defaultValue: '',
-    selectElement: this
+  var $el = $(this);
+  var $parent = $el.parent();
+  var attributes = $el.prop('attributes');
+  var $input = $('<input/>');
+  var selectedValue = $el.val();
+  var typeaheadList = $el.find('option').map(function mapOptions() {
+    if (this.value === '') {
+      // remove any empty values from typeahead
+      return;
+    }
+    return this.value;
+  }).get();
+
+  // remove the selectbox
+  $el.remove();
+
+  $.each(attributes, function applyAttributes() {
+    $input.attr(this.name, this.value);
+  });
+
+  $input.removeClass('js-hidden');
+  $input.addClass('form-control');
+  $input.val(selectedValue);
+
+  $parent.append($input);
+
+  $input.typeahead({
+    hint: false
+  }, {
+    source: new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: typeaheadList,
+      sorter: function sorter(a, b) {
+        var input = $input.val();
+        var startsWithInput = function startsWithInput(x) {
+          return (x.toLowerCase().substr(0, input.length) === input.toLowerCase()) ? -1 : 1;
+        };
+
+        var compareAlpha = function compareAlpha(x, y) {
+          var less = (x < y) ? -1 : 1;
+          return (x === y) ? 0 : less;
+        };
+
+        var compareStartsWithInput = function compareStartsWithInput(x, y) {
+          var startsWithFirst = startsWithInput(x);
+          var startsWithSecond = startsWithInput(y);
+
+          return (startsWithFirst === startsWithSecond) ? 0 : startsWithFirst;
+        };
+
+        var first = compareStartsWithInput(a, b);
+
+        return (first === 0) ? compareAlpha(a, b) : first;
+      }
+    }),
+    limit: 100
   });
 });
-
-// add error styling for select fields that are using accessible-autocomplete
-const autocompleteDiv = $('.autocomplete__wrapper').parent();
-if (autocompleteDiv.parent().hasClass('govuk-form-group--error')) {
-  $('.autocomplete__input').addClass('autocomplete__input--error');
-} else if (!autocompleteDiv.parent().hasClass('govuk-form-group--error')) {
-  $('.autocomplete__input').removeClass('autocomplete__input--error');
-}
 
 /**
  * Override for Start Page width
