e9141edbed57d8efe8cf868d87fce7b86929ad57
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-10-09 11:46:14 +0100

    NRM-432: fix magic link ampersand bug
    - removed the '&' from the link in the getPersonalisation function in email-lookup-sender behaviour
    - refactor the token configuration in check-email-token behaviour so that if req.query.token ended with '?hof-cookie-check', '?hof-cookie-check' would be replaced with empty string.
    - add hof logger to functions to improve logging
    - refactor behaviours to use async with try/catch blocks for better error handling
    - add and refactor relevant tests

--- apps/verify/behaviours/email-lookup-sender.js
+++ apps/verify/behaviours/email-lookup-sender.js
@@ -8,28 +8,28 @@ const templateId = config.govukNotify.templateUserAuthId;
 const appPath = require('../../nrm/index').baseUrl;
 const firstStep = '/start';
 const tokenGenerator = require('../models/save-token');
+const logger = require('hof/lib/logger')({ env: config.env });
 
 const getPersonalisation = (host, token) => {
   return {
-    // pass in `&` at the end in case there is another
-    // query e.g. ?hof-cookie-check
-    'link': `http://${host + appPath + firstStep}?token=${token}&`,
+    'link': `http://${host + appPath + firstStep}?token=${token}`,
     'host': `http://${host}`,
   };
 };
 
-const sendEmail = (email, host, token) => {
-  notifyClient
-    .sendEmail(templateId, email, {
-      personalisation: getPersonalisation(host, token)
-    })
+const sendEmail = async (email, host, token) => {
+  try {
+    await notifyClient
+      .sendEmail(templateId, email, {
+        personalisation: getPersonalisation(host, token)
+      });
     // we will need to log out the response to a proper logger
-    // eslint-disable-next-line no-console
-    .then(console.log(`email sent to ${email}`))
-    .catch(error => {
-      // eslint-disable-next-line no-console
-      console.error(error);
-    });
+    logger.log('info', `email sent to ${email}`);
+  }
+  catch (error) {
+    logger.error(`Error sending email: ${error}`);
+    throw error;
+  };
 };
 
 module.exports = superclass => class extends superclass {
@@ -38,28 +38,35 @@ module.exports = superclass => class extends superclass {
     return config.allowSkip && email === config.skipEmail;
   }
 
-  saveValues(req, res, next) {
+  async saveValues(req, res, next) {
     const email = req.form.values['user-email'];
 
     if (this.skipEmailVerification(email)) {
       return super.saveValues(req, res, next);
     }
-
-    return super.saveValues(req, res, err => {
-      const organisation = req.sessionModel.get('user-organisation');
-      const emailDomain = email.replace(/.*@/, '');
-
-      const isRecognisedEmail = emailDomainCheck.isValidDomain(emailDomain);
-
-      if (!isRecognisedEmail) {
-        req.sessionModel.set('recognised-email', false);
+    return super.saveValues(req, res, async err => {
+      if (err) {
         return next(err);
       }
+      try {
+        const organisation = req.sessionModel.get('user-organisation');
+        const emailDomain = email.replace(/.*@/, '');
 
-      const host = req.get('host');
-      const token = tokenGenerator.save(email, organisation);
-      sendEmail(email, host, token);
-      return next(err);
+        const isRecognisedEmail = emailDomainCheck.isValidDomain(emailDomain);
+
+        if (!isRecognisedEmail) {
+          req.sessionModel.set('recognised-email', false);
+          return next(err);
+        }
+
+        const host = req.get('host');
+        const token = await tokenGenerator.save(email, organisation);
+        await sendEmail(email, host, token);
+        return next();
+      } catch (err) {
+        logger.error('error', `Error in the saveValues method ${error}`);
+        return next(err);
+      }
     });
   }
 
