9226d96b5c90268a17b1b17ed55571692ddb3a40
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2025-02-06 18:12:30 +0000

    NRM-366: Create a Upload supporting evidence and documents page
    - set up file upload model
    - configure file upload fields, validations and pages
    - add /upload-evidence step
    - set save file, remove file, limit doc behaviours
    - create relevant unit tests
    - set up upload and keycloak envs in config.js
    - add file upload to summary-data-sections
    - set up file upload submission
    - update.yarn.lock
    - update readme

---
+++ apps/nrm/models/file-upload.js
@@ -0,0 +1,75 @@
+'use strict';
+/* eslint-disable node/no-deprecated-api */
+
+const url = require('url');
+const { model: Model } = require('hof');
+const uuid = require('uuid').v4;
+const FormData = require('form-data');
+const config = require('../../../config');
+const logger = require('hof/lib/logger')({ env: config.env });
+
+module.exports = class UploadModel extends Model {
+  constructor(...args) {
+    super(...args);
+    this.set('id', uuid());
+  }
+
+  async save() {
+    try {
+      const attributes = {
+        url: config.upload.hostname
+      };
+      const reqConf = url.parse(this.url(attributes));
+      const formData = new FormData();
+      formData.append('document', this.get('data'), {
+        filename: this.get('name'),
+        contentType: this.get('mimetype')
+      });
+      reqConf.data = formData;
+      reqConf.method = 'POST';
+      reqConf.headers = {
+        ...formData.getHeaders()
+      };
+      const result = await new Promise((resolve, reject) => {
+        this.request(reqConf, (err, data) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(data);
+        });
+      });
+      await this.set({ url: result.url.replace('/file/', '/file/generate-link/').split('?')[0]});
+      this.unset('data');
+    } catch (error) {
+      logger.error('Error in save method ', error);
+    }
+  }
+
+  async auth() {
+    try {
+      if (!config.keycloak.token) {
+        logger.error('keycloak token url is not defined');
+        return {
+          bearer: 'abc123'
+        };
+      }
+      const tokenReq = {
+        url: config.keycloak.token,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: {
+          username: config.keycloak.username,
+          password: config.keycloak.password,
+          grant_type: 'password',
+          client_id: config.keycloak.clientId,
+          client_secret: config.keycloak.secret
+        },
+        method: 'POST'
+      };
+      const response = await this._request(tokenReq);
+      return { bearer: response.data.access_token };
+    } catch (error) {
+      logger.error('Error in auth method:', error);
+      throw error;
+    }
+  }
+};
