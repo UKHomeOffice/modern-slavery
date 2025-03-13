a77b3dbf2b091bcc8a7893414a8a88ca1bcfebe2
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2025-03-13 22:00:24 +0000

    test different doc url

--- apps/nrm/models/submission.js
+++ apps/nrm/models/submission.js
@@ -232,8 +232,8 @@ module.exports = (data, token) => {
   data['files'].forEach((doc, i) => {
     console.log('********* DOC', doc);
     const index = i + 1;
-    response[`Document${index}.URL`] = `${doc.url}&token=${token.bearer}`;
-    // response[`Document${index}.URL`] = `${doc.url.replace('/file', '/vault')}&token=${token.bearer}`;
+    // response[`Document${index}.URL`] = `${doc.url}&token=${token.bearer}`;
+    response[`Document${index}.URL`] = `${doc.url.replace('/file', '/vault')}&token=${token.bearer}`;
     // response[`Document${index}.URL`] = doc.url;
     response[`Document${index}.Name`] = doc.name;
     response[`Document${index}.MimeType`] = doc.mimetype;
