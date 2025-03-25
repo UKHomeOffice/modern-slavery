5bcd2cfcf045b095ff988a16e9cd8c804fc326c1
Greg Austin <Gregory.Austin@digital.homeoffice.gov.uk>
2025-03-25 12:09:38 +0000

    NRM-387: "details-last-contact" content updates

--- apps/nrm/fields/index.js
+++ apps/nrm/fields/index.js
@@ -302,6 +302,7 @@ module.exports = {
     mixin: 'textarea',
     'ignore-defaults': true,
     formatter: ['trim', 'hyphens'],
+    labelClassName: 'visuallyhidden',
     validate: ['notUrl', { type: 'maxlength', arguments: 15000 }],
     attributes: [
       {
