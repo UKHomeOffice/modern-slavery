4a95f1591488d2d25546a9f1ced6f2280697f3bc
Gregory Austin <Gregory.Austin@digital.homeoffice.gov.uk>
2025-03-31 09:06:32 +0100

    NRM-387: "details-last-contact" content updates (#742)
    * NRM-387: "details-last-contact" content updates
    
    * NRM-412: Amend content and add new field for /any-other-pvs
    
    - amend intro content
    - add and configure fields
    - add validations
    - add fields to summary page
    - add submission field for icasework
    - amend submission for OtherVictims so that answers appear in plain wording when in icasework
    - add ui tests
    
    * NRM-388: Amend content on /why-report-now page
    
    - amend header content
    - add intro content
    - make label visually hidden
    - amend check your answers label
    - amend validation message to fit amended content
    
    * NRM-385: Update content for taken-somewhere-page (#740)
    
    * NRM-385: Update content for /were-they-taken-somewhere-by-their-exploiter
    
    * NRM-385: "Were they taken" feedback from review
    
    * NRM-392: "pv last in contact" page (#732)
    
    * NRM-390: Amend content for the pv location page
    
    * NRM-375: Update keycloak urls for prod
    
    NRM-375: Update keycloak urls for prod environment
     - Amend lookup and dashboard deployment to point urls to Keycloak PROD realm
    
    * NRM-417: make drone pipeline asynchronous
    
    * NRM-387:"Last contact content updates"
    
    * NRM-387:"Fixing linting: Single quotes"
    
    * NRM-387: "details-last-contact" content updates
    
    * NRM-387:"Last contact content updates"
    
    * NRM-387:"Fixing linting: Single quotes"
    
    * NRM-834 and NRM-378: Amend does-pv-need-support and pv-phone-number pages (#723)
    
    NRM-834: Amend does-pv-need-support page
    - add fork to go to pv-phone-number page
    - amend content in fields.json, pages.json and does-pv-need-support.md
    - rename support example list item properties in pages.json and does-pv-need-support.md for clarity
    - amend ui-integration tests
    
    NRM-378: Amend pv-phone-number page
    
    - amend routing for pv-phone-number page
    - add and config fields for pv-phone-number page
    - reorganise existing fields config on fields, pages and validations.json for pv-phone-number page
    - amend and add summary sections for fields on pv-phone-number page
    - remove maxlength validation for phone-number-yes field as it is replaced by internationalPhoneNumber validator
    - add submission fields for alternative phone number and no contact details and rearrange order of fields to match flow
    - comment out SupportProviderContactByPhone mapping as it will be removed in casework
    - amend SupportProviderContactByPhone submission field to accept pv phone number or alternative phone number
    - add and amend ui integration tests
    
    * Nrm 386 nrm 380 nrm 402 (#729)
    
    * NRM-386: "What is their background" page changes
    
    * NRM-380: "more than one exploitative situation" page
    
    * NRM-402: Create 'multiple exploitations happened' page
    
    * NRM-387: "details-last-contact" content updates
    
    * NRM-387:"Last contact content updates"
    
    * NRM-387:"Fixing linting: Single quotes"
    
    ---------
    
    Co-authored-by: Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
    Co-authored-by: Rhyad Zergane <105197053+Rhyad-Zergane-HO@users.noreply.github.com>
    Co-authored-by: Rhyad Zergane <rhyad.zergane1@digital.homeoffice.gov.uk>
    Co-authored-by: Jon holder <jonathan.holder1@digital.homeoffice.gov.uk>

--- apps/nrm/fields/index.js
+++ apps/nrm/fields/index.js
@@ -302,6 +302,8 @@ module.exports = {
     mixin: 'textarea',
     'ignore-defaults': true,
     formatter: ['trim', 'hyphens'],
+    isPageHeading: true,
+    child: 'partials/details-last-contact',
     validate: ['notUrl', { type: 'maxlength', arguments: 15000 }],
     attributes: [
       {
