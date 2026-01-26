ea4774e2c1fd853bd107ade99bd16be506d71f9d
Vinodha Samiyappan <vinodha.samiyappan@digital.homeoffice.gov.uk>
2026-01-26 14:51:09 +0000

    Updated the secret name

--- test/setup.js
+++ test/setup.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 global.reqres = require('hof').utils.reqres;
 process.env.AWS_SQS = 'test-queue';
 process.env.DELETION_TIMEOUT = 28;
-process.env.SESSION_SECRET = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6';
 
 global.chai = require('chai')
   .use(require('sinon-chai'))
