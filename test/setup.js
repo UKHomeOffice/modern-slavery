6aaea6cc1153e046d5a2e493a4594c919fee7913
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-08-25 12:37:14 +0100

    HOFF-2177 add fallback

--- test/setup.js
+++ test/setup.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 global.reqres = require('hof').utils.reqres;
 process.env.DELETION_TIMEOUT = 28;
 process.env.NOTIFY_STUB = 'true';
-process.env.SAS_HOF_EMAIL = 'test@example.com';
+process.env.SAS_HOF_EMAIL = process.env.SAS_HOF_EMAIL || 'test@example.com';
 global.chai = require('chai')
   .use(require('sinon-chai'))
   .use(require('chai-as-promised'));
