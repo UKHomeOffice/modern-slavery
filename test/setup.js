9d592aba052af203f424ce45fe4b5709a524aaa9
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-08-25 12:40:43 +0100

    use test email for skip email

--- test/setup.js
+++ test/setup.js
@@ -6,6 +6,7 @@ global.reqres = require('hof').utils.reqres;
 process.env.DELETION_TIMEOUT = 28;
 process.env.NOTIFY_STUB = 'true';
 process.env.SAS_HOF_EMAIL = process.env.SAS_HOF_EMAIL || 'test@example.com';
+process.env.SKIP_EMAIL = process.env.SKIP_EMAIL || process.env.SAS_HOF_EMAIL;
 global.chai = require('chai')
   .use(require('sinon-chai'))
   .use(require('chai-as-promised'));
