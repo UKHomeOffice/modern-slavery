c01ec0095b2969dfbdc4956b92400c249aea8e8f
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-09-01 13:43:45 +0100

    NRM-582 clear and disable date fields when checkbox component is ticked

--- apps/nrm/behaviours/pv-dob-validation.js
+++ apps/nrm/behaviours/pv-dob-validation.js
@@ -5,6 +5,20 @@ module.exports = superclass => class extends superclass {
     const { values } = req.form;
     const isDobUnknown = values['pv-dob-not-known'] === true || values['pv-dob-not-known'] === 'true';
 
+    if (isDobUnknown) {
+      values['pv-dob'] = undefined;
+      values['pv-dob-day'] = undefined;
+      values['pv-dob-month'] = undefined;
+      values['pv-dob-year'] = undefined;
+
+      if (req.sessionModel && req.sessionModel.unset) {
+        req.sessionModel.unset('pv-dob');
+        req.sessionModel.unset('pv-dob-day');
+        req.sessionModel.unset('pv-dob-month');
+        req.sessionModel.unset('pv-dob-year');
+      }
+    }
+
     if (!values['pv-dob'] && !isDobUnknown) {
       return next({
         'pv-dob': new this.ValidationError('pv-dob', {
