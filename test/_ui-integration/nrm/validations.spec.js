6c8efa6fd599ed2f088c9b536065d5ac8bf211fb
rayhannurrohmanHO <rayhannur.rohman@digital.homeoffice.gov.uk>
2026-09-21 09:53:07 +0100

    fix failing tests

--- test/_ui-integration/nrm/validations.spec.js
+++ test/_ui-integration/nrm/validations.spec.js
@@ -1139,7 +1139,7 @@ describe('validation checks of the nrm journey', () => {
 
       expect(validationSummary.length === 1).to.be.true;
       expect(validationSummary.html())
-        .to.match(/You must enter a valid date/);
+        .to.match(/Enter a valid date/);
     });
   });
 
